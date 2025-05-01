d76b317ba13edfd8bc71617006193a51b80f2e57
aesadov <aesadov@gmail.com>
2025-05-01 19:45:44 +0300

    refactor: add types in App component

--- vueTodo_3/src/App.vue
+++ vueTodo_3/src/App.vue
@@ -1,21 +1,27 @@
-<script setup>
+<script setup lang='ts'>
   import {ref, computed} from 'vue'
   import AddNewTask from './components/AddNewTask.vue'
   import TodoItem from './components/TodoItem.vue'
 
-  const tasks = ref([])
+  interface TaskType {
+    id: number
+    text: string
+    isDone: boolean
+  }
+
+  const tasks = ref<TaskType[]>([])
   const tasksToDo = computed(() => tasks.value.filter(t => !t.isDone))
   const doneTasks = computed(() => tasks.value.filter(t => t.isDone))
-  const id = ref(0)
+  const id = ref<number>(0)
 
-  function addTask(text) {
+  function addTask(text: string) {
       tasks.value.unshift({id: id.value, text, isDone: false})
       id.value++
   }
-  function removeTask(id){
+  function removeTask(id: number){
     tasks.value = tasks.value.filter(t => t.id !== id)
   }
-  function setTaskIsDone(id) {
+  function setTaskIsDone(id: number) {
     tasks.value = tasks.value.map(t => t.id === id ? {...t, isDone: true} : t)
   }
 </script>
